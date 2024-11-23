937a33b5fe4429e072a8dd1d6107943dbd42080d
KonstantinIstremsky <kalter.storyteller@gmail.com>
2024-11-23 11:11:42 +0300

    HW-14-2

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -1,21 +1,39 @@
 package org.skypro.skyshop.product;
 
-public class Product {
-    private final String name; // Название продукта
-    private final int price;   // Стоимость продукта
+import java.util.Objects;
 
-    // Конструктор
+public class Product {
+    private final String name;
+    private final int price;
+
     public Product(String name, int price) {
         this.name = name;
         this.price = price;
     }
 
-    // Геттеры
     public String getName() {
         return name;
     }
 
     public int getPrice() {
         return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return price == product.price && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+
+    @Override
+    public String toString() {
+        return name + ": " + price;
     }
 }
